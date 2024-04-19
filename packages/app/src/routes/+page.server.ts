fb7e9ab4fad0ab06c290963c72d797238e570907
zeSchlausKwab <schlauskwab@proton.me>
2024-04-19 11:21:28 +0200

    feat: get users in app

---
+++ packages/app/src/routes/+page.server.ts
@@ -0,0 +1,19 @@
+import { db, type Stall } from 'database'
+
+export interface UsersWithStalls {
+	id: string
+	displayName: string
+	stalls: Stall[]
+}
+
+export async function load(): Promise<{ users: UsersWithStalls[] }> {
+	const data = await db.query.users.findMany({
+		with: {
+			stalls: true
+		}
+	})
+
+	return {
+		users: data
+	}
+}
