d93732b6c0e3c62fb9033221eb965f70a5269dcd
gzuuus <gzuuus@protonmail.com>
2024-04-22 13:24:16 +0200

    build: update dependencies feat: added some relations

--- packages/database/schema.ts
+++ packages/database/schema.ts
@@ -1,4 +1,4 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, numeric, primaryKey, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { relations } from "drizzle-orm";
 
 const standardColumns = {
@@ -66,22 +66,25 @@ export const shipping = sqliteTable("shipping", {
   stallId: text("stall_id")
     .notNull()
     .references(() => stalls.id),
-  shippingId: text("shipping_id").primaryKey(),//This sould be a composed primary key (shipping_id, stall_id)
+  shippingId: text("shipping_id"),//This sould be a composed primary key (shipping_id, stall_id)
   name: text("name").notNull(),
   shippingMethod: text("shipping_method").notNull(),
   shippingDetails: text("shipping_details").notNull(),
-  baseCost: integer("base_cost").notNull(),// This should be a decimal
+  baseCost: numeric("base_cost").notNull(),
   isDefault: integer("default").notNull(),
+}, (table) => {
+  return {
+    pk: primaryKey({ columns: [table.bookId, table.authorId] }),// This is not working
+  };
 });
-
 // Shipping zones
 export const shippingZones = sqliteTable("shipping_zones", {
   shippingId: text("shipping_id")
     .notNull()
     .references(() => shipping.shippingId),
   shippingZoneId: text("shipping_zone_id").primaryKey(),
-  regionCode: text("region_code").notNull(),// This should be a enum of regions in ISO 3166 format
-  countryCode: text("country_code").notNull(),// This should be a enum of regions in ISO 3166 format
+  regionCode: text("region_code").notNull(),
+  countryCode: text("country_code").notNull(),
 });
 
 // Products
@@ -98,12 +101,12 @@ export const products = sqliteTable("products", {
   price: integer("price").notNull(),
   productType: text("product_type", { enum: ["simple", "variable", "variation"] }).notNull(),
   images: text("images"),
-  currency: text("currency").notNull(),// This should be a enum of currencies in ISO 3166 format
+  currency: text("currency").notNull(),
   stockQty: integer("stock_qty").notNull(),
   specs: text("specs"),
   shippingCost: integer("shipping_cost"),
   isFeatured: integer("featured", {mode: "boolean"}).notNull(),
-  parentId: text("parent_id").notNull()// We have to self reference this
+  parentId: text("parent_id").notNull()// We have to self reference this to a `id` of another product in the table
 });
 
 // Categories
@@ -111,7 +114,7 @@ export const categories = sqliteTable("categories", {
   catId: integer("cat_id").primaryKey(),
   catName: text("cat_name").notNull(),
   description: text("description").notNull(),
-  parentId: integer("parent_id").notNull()// We have to self reference this
+  parentId: integer("parent_id").notNull()// We have to self reference this to a `id` of another category in the table
 });
 
 // Product categories
@@ -139,7 +142,7 @@ export const auctions = sqliteTable("auctions", {
   startDate: integer("start_date", { mode: "timestamp" }).notNull(),
   endDate: integer("end_date", { mode: "timestamp" }).notNull(),
   images: text("images"),
-  currency: text("currency").notNull(),// This should be a enum of currencies in ISO 3166 format
+  currency: text("currency").notNull(),
   specs: text("specs"),
   shippingCost: integer("shipping_cost"),
   status: text("status").notNull(),
@@ -174,7 +177,7 @@ export const orders = sqliteTable("orders", {
   address: text("address").notNull(),
   zip: text("zip").notNull(),
   city: text("city").notNull(),
-  region: text("region").notNull(),// This should be a enum of regions in ISO 3166 format
+  region: text("region").notNull(),
   contactName: text("contact_name").notNull(),
   contactPhone: text("contact_phone").notNull(),
   contactEmail: text("contact_email").notNull(),
@@ -212,5 +215,78 @@ export const stallRelations = relations(stalls, ({ one }) => ({
   user: one(users, {
     fields: [stalls.userId],
     references: [users.id],
+  }),
+}));
+
+export const productRelations = relations(products, ({ one, many }) => ({
+  user: one(users, {
+    fields: [products.userId],
+    references: [users.id],
+  }),
+  stall: one(stalls, {
+    fields: [products.stallId],
+    references: [stalls.id],
+  }),
+  categories: many(categories),
+}));
+
+export const categoryRelations = relations(categories, ({ many }) => ({
+  products: many(products),
+}));
+
+export const auctionRelations = relations(auctions, ({ one, many }) => ({
+  user: one(users, {
+    fields: [auctions.userId],
+    references: [users.id],
+  }),
+  stall: one(stalls, {
+    fields: [auctions.stallId],
+    references: [stalls.id],
+  }),
+  bids: many(bids),
+}));
+
+export const bidRelations = relations(bids, ({ one }) => ({
+  user: one(users, {
+    fields: [bids.userId],
+    references: [users.id],
+  }),
+  auction: one(auctions, {
+    fields: [bids.auctionId],
+    references: [auctions.id],
+  }),
+}));
+
+export const orderRelations = relations(orders, ({ one, many }) => ({
+  sellerUser: one(users, {
+    fields: [orders.sellerUserId],
+    references: [users.id],
+  }),
+  buyerUser: one(users, {
+    fields: [orders.buyerUserId],
+    references: [users.id],
+  }),
+  shipping: one(shipping, {
+    fields: [orders.shippingId],
+    references: [shipping.shippingId],
+  }),
+  items: many(orderItems),
+}));
+
+export const orderItemRelations = relations(orderItems, ({ one }) => ({
+  order: one(orders, {
+    fields: [orderItems.orderId],
+    references: [orders.id],
+  }),
+  product: one(products, {
+    fields: [orderItems.productId],
+    references: [products.id],
+  }),
+}));
+
+export const invoiceRelations = relations(invoices, ({ one }) => ({
+  order: one(orders, {
+    fields: [invoices.orderId],
+    references: [orders.id],
   }),
 }));
