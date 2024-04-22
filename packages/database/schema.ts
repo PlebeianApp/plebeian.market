22d75028896f487abfda7664a7093d295631f8b6
gzuuus <gzuuus@protonmail.com>
2024-04-22 11:20:51 +0200

    feat: added db:setup script

--- packages/database/schema.ts
+++ packages/database/schema.ts
@@ -1,4 +1,4 @@
-import { integer, sqliteTable, text, SQLiteBoolean } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { relations } from "drizzle-orm";
 
 const standardColumns = {
