1580b5b40c1b21b4e0dd728ad4b028795fac4227
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-04-24 17:16:57 +0330

    init

---
+++ packages/app/src/lib/components/product/item.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index.js'
+</script>
+
+<Card.Root class="cursor-pointer border-4 border-black bg-transparent text-black text-black">
+	<img
+		class="contain h-[329px] object-cover"
+		src="https://searxng.ca/image_proxy?url=https%3A%2F%2Fwww.gearpatrol.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2023%2F09%2Fbest-vintage-film-cameras-refresh-lead-650de2dd54d04-jpg.webp&h=1367be0e2d2f2e23da02bc653c0fcd4867a26a8e426e32a8eb5bc7f9eb21ddb3"
+		alt=""
+	/>
+	<Card.Footer class="flex items-start justify-between p-4">
+		<span class="truncate font-bold">Polaroid Pronto 600</span>
+		<div class="flex flex-col text-right">
+			<span class="font-red font-bold">54k sats</span>
+			<span class="text-sm">$15 USD</span>
+		</div>
+	</Card.Footer>
+</Card.Root>
