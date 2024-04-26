c622d51baa2dafa1176cb1253ff906fdf9c27700
Schlaus Kwab <120575277+zeSchlausKwab@users.noreply.github.com>
2024-04-26 17:02:59 +0200

    fix: stall browser uniform height (#15)

--- packages/app/src/lib/components/stalls/item.svelte
+++ packages/app/src/lib/components/stalls/item.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card/index'
-	import type {RichStall} from '$lib/server/stalls.service'
+	import type { RichStall } from '$lib/server/stalls.service'
 
 	export let item: RichStall
 </script>
 
-<Card.Root class="cursor-pointer border-4 border-black bg-transparent text-black">
+<Card.Root
+	class="flex h-[34vh] cursor-pointer flex-col gap-4 border-4 border-black bg-transparent text-black"
+>
 	<Card.Header class="flex flex-col justify-between">
 		<span class="truncate text-2xl font-bold">{item.name}</span>
 		<span class="font-red font-bold">Since: {item.createDate}</span>
 	</Card.Header>
-	<Card.Content>{item.description}</Card.Content>
+	<Card.Content class="max-h-[33%] flex-grow overflow-hidden">{item.description}</Card.Content>
 
 	<Card.Footer class="flex flex-col items-start font-bold">
 		<span>Owner: {item.userName}</span>
