88c54b92ecc35ae8245c445df9d8aebc69426497
zeSchlausKwab <schlauskwab@proton.me>
2024-04-29 13:51:33 +0200

    config: bump timeouts

--- packages/app/vitest.config.ts
+++ packages/app/vitest.config.ts
@@ -4,8 +4,8 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
 	test: {
-		testTimeout: 10_000,
-		hookTimeout: 10_000,
+		testTimeout: 25_000,
+		hookTimeout: 25_000,
 		globalSetup: './tests/globalSetup.ts',
 		alias: {
 			$lib: new URL('./src/lib', import.meta.url).pathname
