cfce6d299b36ef92c24f6fbedfa6800dbcfa9b53
zeSchlausKwab <schlauskwab@proton.me>
2024-04-30 10:05:40 +0200

    feat: product tile

--- packages/app/src/lib/components/product/item.svelte
+++ packages/app/src/lib/components/product/item.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card/index.js'
 	export let productName: string = 'Polaroid Pronto 600'
-	export let price: string = '$15 USD'
+	export let price: number = 15
+	export let currency: string = 'USD'
 	export let sats: string = '54k sats'
 	export let imageUrl: string =
 		'https://searxng.ca/image_proxy?url=https%3A%2F%2Fwww.gearpatrol.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2023%2F09%2Fbest-vintage-film-cameras-refresh-lead-650de2dd54d04-jpg.webp&h=1367be0e2d2f2e23da02bc653c0fcd4867a26a8e426e32a8eb5bc7f9eb21ddb3'
@@ -13,7 +14,7 @@
 		<span class="truncate font-bold">{productName}</span>
 		<div class="flex flex-col text-right">
 			<span class="font-red font-bold">{sats}</span>
-			<span class="text-sm">{price}</span>
+			<span class="text-sm">${price} {currency}</span>
 		</div>
 	</Card.Footer>
 </Card.Root>
