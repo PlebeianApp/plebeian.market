a757502c31648bbfec433646e55dfe72d534ddf1
gzuuus <gzuuus@protonmail.com>
2024-05-03 18:46:38 +0200

    refactor: prettier

--- packages/app/src/routes/+page.server.ts
+++ packages/app/src/routes/+page.server.ts
@@ -8,17 +8,14 @@ const getHomeProducts = async () => {
 	const productsQuery = db.select().from(products)
 
 	const coolProductsResult = await productsQuery.limit(6).execute()
-	const featuredProductsResult = await db.select({
-		...getTableColumns(products),
-	  })
-	  .from(products)
-	  .innerJoin(productMeta, eq(products.id, productMeta.productId))
-	  .where(
-		and(
-			eq(productMeta.metaName, 'is_global_featured'),
-			eq(productMeta.valueBoolean, true)
-		)
-	).execute();
+	const featuredProductsResult = await db
+		.select({
+			...getTableColumns(products),
+		})
+		.from(products)
+		.innerJoin(productMeta, eq(products.id, productMeta.productId))
+		.where(and(eq(productMeta.metaName, 'is_global_featured'), eq(productMeta.valueBoolean, true)))
+		.execute()
 
 	return {
 		featured: await Promise.all(featuredProductsResult.map(toDisplayProduct)),
