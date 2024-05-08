5d23fc959f9362d037c6a1eb7a525eab061b9ada
zeSchlausKwab <schlauskwab@proton.me>
2024-05-08 12:42:46 +0200

    feat: stall schema and endpoints

--- packages/app/src/routes/api/v1/stalls/+server.ts
+++ packages/app/src/routes/api/v1/stalls/+server.ts
@@ -1,6 +1,31 @@
-import { json } from '@sveltejs/kit'
-import { getAllStalls } from '$lib/server/stalls.service'
+import { NSchema as n } from '@nostrify/nostrify'
+import { error, json } from '@sveltejs/kit'
+import { stallsFilterSchema } from '$lib/schema'
+import { createStall, getAllStalls } from '$lib/server/stalls.service'
+import { verifyEvent } from 'nostr-tools'
 
-export async function GET() {
-	return json(await getAllStalls())
+export async function GET({ url: { searchParams } }) {
+	const spObj = Object.fromEntries(searchParams)
+	const filter = stallsFilterSchema.safeParse(spObj)
+
+	if (!filter.success) {
+		return error(400, `Invalid request: ${JSON.stringify(filter.error)}`)
+	} else {
+		return json(await getAllStalls(filter.data))
+	}
+}
+
+export async function POST({ request }) {
+	const body = await request.json()
+	const verifiedEvent = n
+		.event()
+		.refine(verifyEvent)
+		.refine((val) => val.kind === 30017)
+		.safeParse(body)
+
+	if (!verifiedEvent.success) {
+		return error(400, `Invalid request: ${JSON.stringify(verifiedEvent.error)}`)
+	} else {
+		return json(await createStall(verifiedEvent.data))
+	}
 }
---
+++ packages/app/src/routes/api/v1/stalls/stalls.test.ts
@@ -0,0 +1,80 @@
+import NDK, { NDKEvent, NDKKind, NDKPrivateKeySigner } from '@nostr-dev-kit/ndk'
+import { describe, expect, it } from 'vitest'
+
+import { createId, devUser1 } from '@plebeian/database'
+
+describe('/stalls', () => {
+	it('GET', async () => {
+		const result = await fetch(`http://${process.env.APP_HOST}:${process.env.APP_PORT}/api/v1/stalls`).then((response) => response.json())
+
+		expect(result).toHaveLength(10)
+	})
+
+	it('GET with filter', async () => {
+		const routeParams = {
+			page: '1',
+			pageSize: '15',
+			order: 'desc',
+			orderBy: 'createdAt',
+		}
+
+		const result = await fetch(
+			`http://${process.env.APP_HOST}:${process.env.APP_PORT}/api/v1/stalls?${new URLSearchParams(routeParams)}`,
+		).then((response) => response.json())
+
+		expect(result).toHaveLength(15)
+	})
+
+	it('POST', async () => {
+		const skSigner = new NDKPrivateKeySigner(devUser1.sk)
+		const evContent = {
+			id: createId(),
+			name: 'Hello Stall',
+			description: 'Hello Stall Description',
+			currency: 'USD',
+			quantity: 6,
+			shipping: [
+				{
+					id: Math.random().toString(36).substring(2, 15),
+					name: 'USPS',
+					cost: Math.random() * 10,
+					regions: ['US', 'CA'],
+				},
+			],
+		}
+		const newEvent = new NDKEvent(new NDK({ signer: skSigner }), {
+			kind: 30017 as NDKKind,
+			pubkey: devUser1.pk,
+			content: JSON.stringify(evContent),
+			created_at: Math.floor(Date.now() / 1000),
+			tags: [],
+		})
+
+		await newEvent.sign(skSigner)
+
+		const result = await fetch(`http://${process.env.APP_HOST}:${process.env.APP_PORT}/api/v1/stalls`, {
+			method: 'POST',
+			body: JSON.stringify(newEvent),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).then((response) => response.json())
+
+		expect(result).toStrictEqual({
+			id: expect.any(String),
+			createDate: expect.any(String),
+			currency: 'USD',
+			description: 'Hello Stall Description',
+			name: 'Hello Stall',
+			userId: '86a82cab18b293f53cbaaae8cdcbee3f7ec427fdf9f9c933db77800bb5ef38a0',
+		})
+	})
+
+	it('GET stalls by user id', async () => {
+		const result = await fetch(`http://${process.env.APP_HOST}:${process.env.APP_PORT}/api/v1/stalls?userId=testUserId`).then((response) =>
+			response.json(),
+		)
+
+		expect(result).toHaveLength(10)
+	})
+})
