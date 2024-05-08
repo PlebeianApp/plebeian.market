7d50ffd77812bdfd374d16084024b73f373502d4
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-05-08 13:03:20 +0330

    feat: iconify icons (#31)
    * resolve reviews
    
    * chore: resolve reviews
    
    * chore: resolve reviews
    
    * chore: resolve reviews

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,5 +1,18 @@
+import { iconsPlugin, getIconCollections } from '@egoist/tailwindcss-icons'
+
 /** @type {import('tailwindcss').Config} */
 const config = {
+	plugins: [
+    iconsPlugin({
+      // Select the icon collections you want to use
+      // You can also ignore this option to automatically discover all individual icon packages you have installed
+      // If you install @iconify/json, you should explicitly specify the collections you want to use, like this:
+      collections: getIconCollections(["game-icons", "ion", "simple-icons", "mingcute", "tdesign"]),
+      // If you want to use all icons from @iconify/json, you can do this:
+      // collections: getIconCollections("all"),
+      // and the more recommended way is to use `dynamicIconsPlugin`, see below.
+    }),
+	],
 	darkMode: ['class'],
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	safelist: ['dark'],
