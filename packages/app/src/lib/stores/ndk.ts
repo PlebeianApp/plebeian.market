e6ecca3ca44db48d83b06c21e53474beac8186b1
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-05-08 18:49:24 +0330

    chore: instantiate ndk (#41)
    * chore: init
    
    * refactor: front end (#40)
    
    fix: tests
    
    * refactor: export ndk as store, defaultRelaysUrls list
    
    ---------
    
    Co-authored-by: gzuuus <gzuuus@protonmail.com>

---
+++ packages/app/src/lib/stores/ndk.ts
@@ -0,0 +1,25 @@
+import type { NDKCacheAdapter } from '@nostr-dev-kit/ndk'
+import NDKCacheAdapterDexie from '@nostr-dev-kit/ndk-cache-dexie'
+import NDKSvelte from '@nostr-dev-kit/ndk-svelte'
+import { writable } from 'svelte/store'
+
+let cacheAdapter: NDKCacheAdapter | undefined = undefined
+
+if (typeof window !== 'undefined') {
+	cacheAdapter = new NDKCacheAdapterDexie({
+		dbName: 'plebeian.v0',
+	})
+}
+
+export const defaulRelaysUrls: string[] = ['wss://purplepag.es', 'wss://relay.nostr.band', 'wss://nos.lol', 'wss://bouncer.nostree.me']
+
+export const ndk: NDKSvelte = new NDKSvelte({
+	explicitRelayUrls: defaulRelaysUrls,
+	cacheAdapter,
+})
+
+ndk.connect().then(() => console.log('ndk connected successfully'))
+
+const ndkStore = writable(ndk)
+
+export default ndkStore
