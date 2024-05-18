c74cecf7497aaa6535347adfb6d4908406f9e9a5
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-05-18 11:22:54 +0330

    debug

--- packages/app/vitest.config.ts
+++ packages/app/vitest.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
 	test: {
 		testTimeout: 25_000,
 		hookTimeout: 25_000,
-		globalSetup: './tests/globalSetup.ts',
 		alias: {
 			$lib: new URL('./src/lib', import.meta.url).pathname,
 		},
