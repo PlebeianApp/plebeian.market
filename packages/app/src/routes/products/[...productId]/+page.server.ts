51141b547119bd251515c73f1128b5f50f86fc7c
gzuuus <gzuuus@protonmail.com>
2024-05-21 14:31:01 +0200

    refactor: added typos and throw

--- packages/app/src/routes/products/[...productId]/+page.server.ts
+++ packages/app/src/routes/products/[...productId]/+page.server.ts
@@ -6,16 +6,22 @@ import { NIP05_REGEX } from 'nostr-tools/nip05'
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ params }) => {
-	let userId
-	let productSlug
+	let userId: string | undefined
+	let productSlug: string | undefined
 
-	const [nip05, productId] = params.productId.split('/')
-	if (NIP05_REGEX.test(nip05) && productId) {
-		userId = (await getUserByNip05(nip05)).id
+	const parts: string[] = params.productId.split('/')
+	if (parts.length < 1 || parts.length > 2) {
+		throw new Error('Invalid productId format')
+	}
+
+	const [root, productId] = parts
+
+	if (NIP05_REGEX.test(root) && productId) {
+		userId = (await getUserByNip05(root)).id
 		productSlug = productId
 	}
 
-	const product = userId && productSlug ? await getProductById(`${KindProducts}:${userId}:${productSlug}`) : await getProductById(nip05)
+	const product = userId && productSlug ? await getProductById(`${KindProducts}:${userId}:${productSlug}`) : await getProductById(root)
 	const seller = await getUserForProduct(product.id)
 	const products = (await getProductsByUserId(seller.id)).slice(0, 4)
 
