0852029d26ce8455597853b56792376486d234da
zeSchlausKwab <schlauskwab@proton.me>
2024-06-01 22:57:52 +0200

    feat: deletion endpoint and test

--- packages/app/src/routes/api/v1/users/[userId]/+server.ts
+++ packages/app/src/routes/api/v1/users/[userId]/+server.ts
@@ -1,63 +1,64 @@
 import { error, json } from '@sveltejs/kit'
-import { decodeJwtToEvent } from '$lib/server/nostrAuth.service'
+import { authorize } from '$lib/auth'
 import { deleteUser, getUserById, updateUser } from '$lib/server/users.service'
-import { findCustomTags } from '$lib/utils'
 
 import type { RequestHandler } from './$types'
 
 export const GET: RequestHandler = async ({ params, request }) => {
-	const authorizationHeader = request.headers.get('Authorization')
+	const { userId } = params
 
-	if (authorizationHeader) {
-		const token = decodeJwtToEvent(authorizationHeader)
-		if (token.pubkey === params.userId && findCustomTags(token.tags, 'method')[0] === request.method) {
-			return json(await getUserById(params.userId))
+	try {
+		await authorize(request, userId, 'GET')
+		return json(await getUserById(userId))
+	} catch (e) {
+		if (e.status === 401) {
+			const user = await getUserById(userId)
+			const userUnAuthResponse = {
+				id: user.id,
+				created_at: user.createdAt.getTime() / 1000,
+				name: user.name,
+				displayName: user.displayName,
+				about: user.about,
+				image: user.image,
+				banner: user.banner,
+				nip05: user.nip05,
+				lud06: user.lud06,
+				lud16: user.lud16,
+				zapService: user.zapService,
+				website: user.website,
+				trustLevel: user.trustLevel,
+			}
+			return json(userUnAuthResponse)
 		}
-
-		return error(401, 'Invalid Token')
+		throw e
 	}
-
-	const user = await getUserById(params.userId)
-
-	const userUnAuthResponse = {
-		id: user.id,
-		created_at: user.createdAt.getTime() / 1000,
-		name: user.name,
-		displayName: user.displayName,
-		about: user.about,
-		image: user.image,
-		banner: user.banner,
-		nip05: user.nip05,
-		lud06: user.lud06,
-		lud16: user.lud16,
-		zapService: user.zapService,
-		website: user.website,
-		trustLevel: user.trustLevel,
-	}
-
-	return json(userUnAuthResponse)
 }
 
 export const PUT: RequestHandler = async ({ params, request }) => {
-	const authorizationHeader = request.headers.get('Authorization')
+	const { userId } = params
 
-	if (!authorizationHeader) {
-		return error(401, 'Invalid Token')
-	}
-
-	const token = decodeJwtToEvent(authorizationHeader)
-	if (token.pubkey === params.userId && findCustomTags(token.tags, 'method')[0] === request.method) {
-		try {
-			const body = await request.json()
-			return json(await updateUser(params.userId, body))
-		} catch (e) {
-			error(500, JSON.stringify(e))
+	try {
+		await authorize(request, userId, 'PUT')
+		const body = await request.json()
+		return json(await updateUser(userId, body))
+	} catch (e) {
+		if (e.status) {
+			return error(e.status, e.message)
 		}
+		return error(500, JSON.stringify(e))
 	}
-
-	return error(401, 'Invalid Token')
 }
 
-export const DELETE: RequestHandler = async ({ params }) => {
-	return json(await deleteUser(params.userId))
+export const DELETE: RequestHandler = async ({ params, request }) => {
+	const { userId } = params
+
+	try {
+		await authorize(request, userId, 'DELETE')
+		return json(await deleteUser(userId))
+	} catch (e) {
+		if (e.status) {
+			return error(e.status, e.message)
+		}
+		return error(500, JSON.stringify(e))
+	}
 }
