1a0812a795722c3f8db364bb2a23e403f3d6f13c
plebomoto <plebomoto@proton.me>
2024-06-26 17:50:31 +0200

    feat: reactive user page

--- packages/app/src/routes/p/[id]/+page.svelte
+++ packages/app/src/routes/p/[id]/+page.svelte
@@ -6,6 +6,9 @@
 	import { Avatar, AvatarFallback, AvatarImage } from '$lib/components/ui/avatar'
 	import Button from '$lib/components/ui/button/button.svelte'
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu'
+	import { createCategoriesByFilterQuery } from '$lib/fetch/category.queries'
+	import { createProductsByFilterQuery } from '$lib/fetch/products.queries'
+	import { createStallsByFilterQuery } from '$lib/fetch/stalls.queries'
 	import { openDrawerForNewProduct, openDrawerForNewStall } from '$lib/stores/drawer-ui'
 	import ndkStore from '$lib/stores/ndk'
 	import { copyToClipboard } from '$lib/utils'
@@ -13,9 +16,19 @@
 	import type { PageData } from './$types'
 
 	export let data: PageData
-	$: ({ npub, pubkey, name, image, products, stalls, categories } = data)
+	$: ({ npub, pubkey, name, image } = data)
 
 	let isMe = false
+
+	$: categoriesQuery = createCategoriesByFilterQuery({ userId: $ndkStore.activeUser?.pubkey })
+
+	$: stallsQuery = createStallsByFilterQuery({
+		userId: $ndkStore.activeUser?.pubkey,
+	})
+
+	$: productsQuery = createProductsByFilterQuery({
+		userId: $ndkStore.activeUser?.pubkey,
+	})
 
 	$: {
 		const userId = $ndkStore.activeUser?.pubkey
@@ -53,24 +66,24 @@
 					</div>
 				</div>
 			</div>
-			{#if categories.length}
+			{#if $categoriesQuery.data}
 				<div class="py-5 lg:px-12">
 					<div class="container">
 						<h2>Categories</h2>
 						<div class=" grid grid-cols-4 gap-2">
-							{#each categories.filter((cat) => (cat.productCount ?? 0) > 0) as cat}
+							{#each $categoriesQuery.data.filter((cat) => (cat.productCount ?? 0) > 0) as cat}
 								<CatCompactItem {cat} isGlobal={false} />
 							{/each}
 						</div>
 					</div>
 				</div>
 			{/if}
-			{#if stalls.length}
+			{#if $stallsQuery.data}
 				<div class="px-4 py-20 lg:px-12">
 					<div class="container">
 						<h2>Stalls</h2>
 						<div class="grid auto-cols-max grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-							{#each stalls as item}
+							{#each $stallsQuery.data as item}
 								<StallItem stall={item} />
 							{/each}
 						</div>
@@ -78,12 +91,12 @@
 				</div>
 			{/if}
 
-			{#if products.length}
+			{#if $productsQuery.data}
 				<div class="px-4 py-20 lg:px-12">
 					<div class="container">
 						<h2>Products</h2>
 						<div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-							{#each products as item}
+							{#each $productsQuery.data as item}
 								<ProductItem product={item} />
 							{/each}
 						</div>
