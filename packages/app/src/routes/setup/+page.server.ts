f829cade083efe59fff3c44b9bb76e4c8c546684
gzuuus <gzuuus@protonmail.com>
2024-07-03 18:42:24 +0200

    fix: env vars

--- packages/app/src/routes/setup/+page.server.ts
+++ packages/app/src/routes/setup/+page.server.ts
@@ -1,14 +1,18 @@
-import { INSTANCE_PASS } from '$env/static/private'
 import { usersFilterSchema } from '$lib/schema'
 import { getUsersByRole } from '$lib/server/users.service'
+import dotenv from 'dotenv'
 
 import { CURRENCIES, USER_ROLES } from '@plebeian/database/constants'
 
 import type { PageServerLoad } from './$types'
 
+dotenv.config({
+	path: '../../.env',
+})
+
 export const load: PageServerLoad = async () => {
 	const currencies = CURRENCIES
 	const adminUsers = await getUsersByRole(usersFilterSchema.parse({ role: USER_ROLES.ADMIN }))
-	const instancePass = INSTANCE_PASS
+	const instancePass = process.env.INSTANCE_PASS
 	return { currencies, adminUsers, instancePass }
 }
