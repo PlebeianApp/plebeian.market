df6d84fa3cb51d9190a1d10583ea1dfe612ef65d
plebomoto <plebomoto@proton.me>
2024-07-12 14:13:00 +0200

    feat: font

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -3,15 +3,15 @@ import { iconsPlugin, getIconCollections } from '@egoist/tailwindcss-icons'
 /** @type {import('tailwindcss').Config} */
 const config = {
 	plugins: [
-    iconsPlugin({
-      // Select the icon collections you want to use
-      // You can also ignore this option to automatically discover all individual icon packages you have installed
-      // If you install @iconify/json, you should explicitly specify the collections you want to use, like this:
-      collections: getIconCollections(["game-icons", "ion", "simple-icons", "mingcute", "tdesign", "mdi"]),
-      // If you want to use all icons from @iconify/json, you can do this:
-      // collections: getIconCollections("all"),
-      // and the more recommended way is to use `dynamicIconsPlugin`, see below.
-    }),
+		iconsPlugin({
+			// Select the icon collections you want to use
+			// You can also ignore this option to automatically discover all individual icon packages you have installed
+			// If you install @iconify/json, you should explicitly specify the collections you want to use, like this:
+			collections: getIconCollections(["game-icons", "ion", "simple-icons", "mingcute", "tdesign", "mdi"]),
+			// If you want to use all icons from @iconify/json, you can do this:
+			// collections: getIconCollections("all"),
+			// and the more recommended way is to use `dynamicIconsPlugin`, see below.
+		}),
 	],
 	darkMode: ['class'],
 	content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -21,7 +21,7 @@ const config = {
 			center: true,
 			padding: '2rem',
 			screens: {
-				'2xl': '1400px',
+				'2xl': '1600px',
 			},
 		},
 		extend: {
@@ -69,7 +69,7 @@ const config = {
 				base: '4px 4px 0px 0px rgba(0,0,0,1)',
 			},
 			fontFamily: {
-				sans: ['"DM Sans"', 'sans-serif'],
+				sans: ['"IBM Plex Mono"', 'monospace'],
 			},
 		},
 	},
