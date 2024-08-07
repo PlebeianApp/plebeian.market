46659b08e0ee064eb203c303c9185e30a54b40ab
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-08-07 12:20:08 +0330

    init

--- packages/app/src/routes/checkout/+page.svelte
+++ packages/app/src/routes/checkout/+page.svelte
@@ -1,37 +1,18 @@
 <script lang="ts">
-	import GrandTotalDisplay from '$lib/components/cart/grand-total-display.svelte'
-	import Order from '$lib/components/cart/order.svelte'
-	import CheckoutForm from '$lib/components/checkout-form.svelte'
 	import Stepper from '$lib/components/stepper.svelte'
-	import Button from '$lib/components/ui/button/button.svelte'
-	import { cart } from '$lib/stores/cart'
+	import Review from '$lib/components/checkout-review.svelte'
 
 	let currentStep = 0
 
 	$: exampleSteps = [
-		...Object.values($cart.users).map((user) => ({
-			component: Order,
-			props: {
-				user,
-				stalls: $cart.stalls,
-				products: $cart.products,
-				mode: 'checkout',
-			},
-		})),
 		{
-			component: CheckoutForm,
-			props: {},
-		},
-		{
-			component: GrandTotalDisplay,
+			component: Review,
 			props: {},
 		},
 	]
 </script>
 
-<div class="container">
+<div class="container py-6">
 	<h2>Checkout</h2>
 	<Stepper steps={exampleSteps} {currentStep} />
-
-	<Button class="mt-6" disabled={currentStep === exampleSteps.length - 1} on:click={() => currentStep++}>Next Step</Button>
 </div>
