7a6e6f407a914b1a876766e9a7c74568b48387ae
gzuuus <gzuuus@protonmail.com>
2024-08-11 17:47:10 +0200

    refactor: general improvements

--- packages/app/src/lib/components/assets/relayList.svelte
+++ packages/app/src/lib/components/assets/relayList.svelte
@@ -2,84 +2,72 @@
 	import type { NDKRelay } from '@nostr-dev-kit/ndk'
 	import { NDKRelayStatus } from '@nostr-dev-kit/ndk'
 	import ndkStore from '$lib/stores/ndk'
+	import { onMount } from 'svelte'
 
 	export let relay: NDKRelay
 	export let expanded = false
 	export let relayType: 'outbox' | 'kind3' = 'kind3'
+
 	let notices: string[] = []
-	$: relay.on('notice', (notice) => {
+	$: relayConnectivity = relay.connectivity
+
+	const noticeHandler = (notice: string) => {
 		notices = [notice, ...notices]
+	}
+
+	onMount(() => {
+		relay.on('notice', noticeHandler)
+		relay.on('connect', () => (relayConnectivity = relay.connectivity))
+		relay.on('disconnect', () => (relayConnectivity = relay.connectivity))
+		relay.on('flapping', () => (relayConnectivity = relay.connectivity))
+
+		return () => {
+			relay.off('notice', noticeHandler)
+			relay.off('connect', () => (relayConnectivity = relay.connectivity))
+			relay.off('disconnect', () => (relayConnectivity = relay.connectivity))
+			relay.off('flapping', () => (relayConnectivity = relay.connectivity))
+		}
 	})
 
-	$: activeSubCount = relay.connectivity.openSubs.size
-	$: reactiveStatus = relay.connectivity.status
-	setInterval(() => {
-		activeSubCount = relay.connectivity.openSubs.size
-		reactiveStatus = relay.connectivity.status
-	}, 2000)
-
-	let nextReconnectIn: number | undefined
-	let nextReconnectInterval: number
-
-	setInterval(() => {
-		if (!relay.connectionStats.nextReconnectAt || relay.connectionStats.nextReconnectAt! < Date.now()) {
-			clearInterval(nextReconnectInterval)
-			nextReconnectIn = undefined
-			return
-		}
-
-		nextReconnectIn = Math.floor((relay.connectionStats.nextReconnectAt - Date.now()) / 1000)
-	}, 1000)
-
-	function handleRemoveRelay(relay: NDKRelay) {
+	function handleRemoveRelay() {
 		const removeRelay = relayType == 'kind3' ? $ndkStore.pool.removeRelay(relay.url) : $ndkStore.outboxPool?.removeRelay(relay.url)
 		removeRelay && ndkStore.set($ndkStore)
 	}
 </script>
 
-<li class=" list-none flex flex-col">
-	<div class=" inline-flex items-center gap-2">
-		{#if reactiveStatus === NDKRelayStatus.CONNECTING || reactiveStatus === NDKRelayStatus.RECONNECTING}
-			<span class="w-2 h-4 rounded-full bg-orange-400 ml-2" />
-		{:else if reactiveStatus === NDKRelayStatus.DISCONNECTED}
-			<span class="w-2 h-4 rounded-full bg-red-500 ml-2" />
-		{:else if reactiveStatus === NDKRelayStatus.CONNECTED}
-			<span class="w-2 h-4 rounded-full bg-green-500 ml-2" />
-		{:else if reactiveStatus === NDKRelayStatus.FLAPPING}
-			<span class="w-2 h-4 rounded-full bg-blue-500 ml-2" />
-		{:else if reactiveStatus === NDKRelayStatus.AUTHENTICATING}
-			<span class="w-2 h-4 rounded-full bg-red-600 ml-2" />
-		{/if}
+<li class="list-none flex flex-col">
+	<div class="inline-flex items-center gap-2">
+		<span
+			class="w-2 h-4 rounded-full ml-2"
+			class:bg-orange-400={relayConnectivity.status === NDKRelayStatus.CONNECTING ||
+				relayConnectivity.status === NDKRelayStatus.RECONNECTING}
+			class:bg-red-500={relayConnectivity.status === NDKRelayStatus.DISCONNECTED}
+			class:bg-green-500={relayConnectivity.status === NDKRelayStatus.CONNECTED}
+			class:bg-blue-500={relayConnectivity.status === NDKRelayStatus.FLAPPING}
+			class:bg-red-600={relayConnectivity.status === NDKRelayStatus.AUTHENTICATING}
+		/>
 
 		<button
 			class="flex items-center gap-2 w-full bg-transparent border-0 p-0 font-inherit cursor-pointer text-left"
 			on:click={() => (expanded = !expanded)}
 		>
 			<span class="font-normal whitespace-nowrap overflow-hidden text-ellipsis">{relay.url}</span>
-			{#if activeSubCount > 0}
+			{#if relayConnectivity.openSubs.size > 0}
 				<div class="ml-2 float-right text-sm font-light cursor-pointer">
-					{activeSubCount}
-					{activeSubCount === 1 ? '+' : '++'}
+					{relayConnectivity.openSubs.size}{relayConnectivity.openSubs.size === 1 ? '+' : '++'}
 				</div>
 			{/if}
 		</button>
-		<button type="button" on:click={() => handleRemoveRelay(relay)}>
-			<span class=" i-tdesign-remove" />
+		<button type="button" on:click={handleRemoveRelay}>
+			<span class="i-tdesign-remove" />
 		</button>
 	</div>
 
-	{#if relay.connectionStats.attempts > 1 && relay.status !== NDKRelayStatus.CONNECTED}
+	{#if relay.connectionStats.attempts > 1 && relayConnectivity.status !== NDKRelayStatus.CONNECTED}
 		<div class="text-sm font-light mt-2">
 			<small>
 				Reconnection attempts: {relay.connectionStats.attempts}
 			</small>
-
-			{#if nextReconnectIn}
-				<small>
-					Next reconnect in
-					{nextReconnectIn} seconds
-				</small>
-			{/if}
 		</div>
 	{/if}
 	{#if expanded}
--- packages/app/src/lib/components/assets/relayWidget.svelte
+++ packages/app/src/lib/components/assets/relayWidget.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { NDKRelay, NDKRelayAuthPolicies, normalizeRelayUrl } from '@nostr-dev-kit/ndk'
-	import ndkStore from '$lib/stores/ndk'
-	import { onMount } from 'svelte'
+	import ndkStore, { defaulRelaysUrls } from '$lib/stores/ndk'
 
 	import Button from '../ui/button/button.svelte'
 	import Input from '../ui/input/input.svelte'
@@ -9,17 +8,8 @@
 	import RelayList from './relayList.svelte'
 
 	export let mode: 'widget' | 'settings' = 'widget'
-	let isOpen = false
+	let isOpen = mode == 'settings'
 
-	function toggleOpen() {
-		isOpen = !isOpen
-	}
-
-	onMount(() => {
-		mode == 'settings' && (isOpen = true)
-	})
-
-	$: containerStyle = mode == 'widget' ? ' w-fit p-2 border-black border-2 max-w-[25vw]' : 'flex flex-col gap-2'
 	let inputRelayUrl = ''
 	function handleAddRelay() {
 		if (inputRelayUrl) {
@@ -28,48 +18,54 @@
 			inputRelayUrl = ''
 		}
 	}
+
+	function handleConnectToDefaultRelays() {
+		defaulRelaysUrls.forEach((relay) => {
+			$ndkStore.pool.addRelay(new NDKRelay(normalizeRelayUrl(relay), NDKRelayAuthPolicies.signIn(), $ndkStore))
+		})
+		ndkStore.set($ndkStore)
+	}
 </script>
 
-<div class="flex flex-col" class:gap-4={mode == 'settings'}>
-	{#if mode == 'widget'}
-		<Button size="icon" variant="secondary" class=" border border-gray" on:click={toggleOpen}>
-			<span class=" i-mdi-connection w-5 h-5" />
+<div class="flex flex-col" class:gap-4={mode === 'settings'}>
+	{#if mode === 'widget'}
+		<Button size="icon" variant="secondary" class="border border-gray" on:click={() => (isOpen = !isOpen)}>
+			<span class="i-mdi-connection w-5 h-5" />
 		</Button>
 	{/if}
 
 	{#if isOpen}
-		<div class={`bg-white ${containerStyle} `}>
+		<div class={`bg-white flex flex-col gap-2 ${mode == 'widget' && 'w-fit p-2 border-black border-2 max-w-[25vw]'}`}>
 			{#if $ndkStore.pool?.relays.size}
 				<section>
 					Explicit relays:
-					{#each $ndkStore.pool.relays.values() as relay}
+					{#each $ndkStore.pool.relays.values() as relay (relay.url)}
 						<RelayList {relay} relayType="kind3" />
 					{/each}
 				</section>
 			{:else}
-				No connected relay
+				<div class="flex flex-col gap-2">
+					You are not connected to any relay
+					<Button on:click={handleConnectToDefaultRelays}>Use default relays</Button>
+
+					<!-- <small>*You can configure your relays in the <a href="/settings/account/network">settings</a> </small> -->
+				</div>
 			{/if}
 			{#if $ndkStore.outboxPool?.relays.size}
 				<section>
 					Outbox relays:
-					{#each $ndkStore.outboxPool.relays as relay}
-						<RelayList relay={relay[1]} relayType="outbox" />
+					{#each $ndkStore.outboxPool.relays.values() as relay (relay.url)}
+						<RelayList {relay} relayType="outbox" />
 					{/each}
 				</section>
 			{/if}
+			<form on:submit|preventDefault={handleAddRelay} class="flex flex-row gap-2 items-end w-full">
+				<Label class="w-full flex flex-col gap-2">
+					Add a relay
+					<Input name="relayUrl" bind:value={inputRelayUrl} type="url" />
+				</Label>
+				<Button type="submit"><span class="i-tdesign-add text-black w-6 h-6"></span></Button>
+			</form>
 		</div>
-	{/if}
-	{#if mode == 'settings'}
-		<form on:submit|preventDefault={handleAddRelay} class="flex flex-row gap-2 items-end w-full">
-			<Label class="w-full flex flex-col gap-2">
-				Add a relay
-				<Input name="relayUrl" bind:value={inputRelayUrl} type="url" />
-			</Label>
-			<Button type="submit"><span class=" i-tdesign-add text-black w-6 h-6"></span></Button>
-		</form>
-		<section class=" w-full flex justify-center gap-4">
-			<Button disabled type="submit"><span class=" i-tdesign-add text-black w-6 h-6" /> Publish explicit relays</Button>
-			<Button disabled type="submit"><span class=" i-tdesign-add text-black w-6 h-6" /> Publish outbox relays</Button>
-		</section>
 	{/if}
 </div>
