b3fa893282bc0875164bbb318159d06269e984c9
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-08-13 17:50:18 +0330

    feat: checkout success (#223)

---
+++ packages/app/src/lib/components/checkout/success.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte'
+	import { CheckCircle } from 'lucide-svelte'
+</script>
+
+<div class="flex flex-col items-center justify-center h-full gap-8">
+	<CheckCircle class="w-24 h-24 mt-8" />
+	<span class="text-xl">Your order has been sent successfully!</span>
+
+	<Button>Continue <span class="ml-2 i-tdesign-arrow-right"></span></Button>
+</div>
--- packages/app/src/routes/checkout/+page.svelte
+++ packages/app/src/routes/checkout/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import Review from '$lib/components/checkout/review.svelte'
+	import Success from '$lib/components/checkout/success.svelte'
 	import Stepper from '$lib/components/stepper.svelte'
-
-	let currentStep = 0
+	import { currentStep } from '$lib/stores/checkout'
 
 	$: exampleSteps = [
 		{
 			component: Review,
+			props: {},
+		},
+		{
+			component: Success,
 			props: {},
 		},
 	]
@@ -14,5 +18,5 @@
 
 <div class="container py-6">
 	<h2>Checkout</h2>
-	<Stepper steps={exampleSteps} {currentStep} />
+	<Stepper steps={exampleSteps} currentStep={$currentStep} />
 </div>
