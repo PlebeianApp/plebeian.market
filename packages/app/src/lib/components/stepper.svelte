b7cc9ee998d3cf1a6975b17756034d6bcbe1cd56
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-08-17 13:53:38 +0330

    update

--- packages/app/src/lib/components/stepper.svelte
+++ packages/app/src/lib/components/stepper.svelte
@@ -3,8 +3,6 @@
 	import type { Writable } from 'svelte/store'
 	import { writable } from 'svelte/store'
 
-	import Button from './ui/button/button.svelte'
-
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	type Constructor<T> = new (...args: any[]) => T
 
@@ -21,26 +19,6 @@
 	export let steps: Step[] = []
 	export let currentStep: Writable<number> = writable(0)
 </script>
-
-<div class="w-full flex items-center">
-	{#each steps as _, index}
-		<div class="flex items-center">
-			<div class="relative">
-				<Button
-					variant={'secondary'}
-					class={`w-8 h-8 p-0 flex items-center justify-center rounded-full ${index === $currentStep ? 'border-2 border-primary' : 'border-none'} ${index < $currentStep ? 'bg-primary' : 'bg-secondary'}`}
-					on:click={() => currentStep.set(index)}
-				>
-					{index + 1}
-				</Button>
-				{#if index < steps.length - 1}
-					<div class="absolute top-1/2 w-full border-t-2 border-gray-200 left-8"></div>
-				{/if}
-			</div>
-			<div class="ml-2"></div>
-		</div>
-	{/each}
-</div>
 
 <div class="w-full bg-gray-200 rounded-full h-2.5 mt-4">
 	<div class="bg-green-500 h-2.5 rounded-full" style="width: {($currentStep / (steps.length - 1)) * 100}%"></div>
