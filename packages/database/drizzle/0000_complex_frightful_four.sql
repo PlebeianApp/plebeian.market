a220bf32bf97555a81c63dd4d91750845fdaa893
gzuuus <gzuuus@protonmail.com>
2024-08-20 16:54:35 +0200

    refactor: drizz

--- packages/database/drizzle/0000_complex_frightful_four.sql
+++ packages/database/drizzle/0000_complex_frightful_four.sql
@@ -88,7 +88,8 @@ CREATE TABLE `invoices` (
 	`order_id` text NOT NULL,
 	`total_amount` numeric NOT NULL,
 	`invoice_status` text DEFAULT 'pending' NOT NULL,
-	`payment_details_id` text NOT NULL,
+	`payment_details_id` text,
+	`concept` text,
 	FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`) ON UPDATE cascade ON DELETE cascade,
 	FOREIGN KEY (`payment_details_id`) REFERENCES `payment_details`(`id`) ON UPDATE no action ON DELETE no action
 );
