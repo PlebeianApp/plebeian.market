e7a2d057095ec540107277c03bc44b0aa432cdb6
gzuuus <gzuuus@protonmail.com>
2024-09-10 20:45:37 +0200

    fix: init

---
+++ packages/app/src/routes/checkout/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import MustLogin from '$lib/components/assets/mustLogin.svelte'
+	import ndkStore from '$lib/stores/ndk'
+</script>
+
+{#if $ndkStore.activeUser}
+	<slot />
+{:else}
+	<div class="flex justify-center p-24">
+		<MustLogin />
+	</div>
+{/if}
