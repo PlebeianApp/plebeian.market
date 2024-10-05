a72b9841b00cd73cdc30a815eb8133ebdb2d50ea
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-10-05 17:37:54 +0330

    update

--- packages/app/src/routes/dash/activity/+page.svelte
+++ packages/app/src/routes/dash/activity/+page.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
-	import { redirect } from '@sveltejs/kit'
 	import { goto } from '$app/navigation'
 	import ConversationListItem from '$lib/components/chat/conversation-list-item.svelte'
 	import OrderTable from '$lib/components/order/order-table/order-table.svelte'
-	import { createOrdersByUserAndRoleQuery } from '$lib/fetch/order.queries'
-	import { groupedDMs, unseenDMs } from '$lib/nostrSubs/subs'
+	import { unseenDMs } from '$lib/nostrSubs/subs'
 	import { pendingPurchasedOrders, pendingSalesOrders } from '$lib/stores/activity'
-	import ndkStore from '$lib/stores/ndk'
-
-	import { ORDER_STATUS } from '@plebeian/database/constants'
-
-	// $: purchasedOrderQuery = createOrdersByUserAndRoleQuery($ndkStore.activeUser?.pubkey ?? '', 'buyer')
-	// $: pendingPurchasedOrders = $purchasedOrderQuery.data?.orders.filter((order) => order.status === ORDER_STATUS.PENDING)
-
-	// $: salesOrderQuery = createOrdersByUserAndRoleQuery($ndkStore.activeUser?.pubkey ?? '', 'seller')
-	// $: pendingSalesOrders = $salesOrderQuery.data?.orders.filter((order) => order.status === ORDER_STATUS.PENDING)
 </script>
 
 <div class="pb-4 space-y-2">
