63d0cc31c6aac4e21e6ede629b9bfe4acc9d5d42
gzuuus <gzuuus@protonmail.com>
2024-11-11 17:46:14 +0100

    refactor: try catch block and returning boolean in `publishEvent`

--- packages/app/src/lib/utils/nostr.utils.ts
+++ packages/app/src/lib/utils/nostr.utils.ts
@@ -1,9 +1,15 @@
 import type { NDKEvent } from '@nostr-dev-kit/ndk'
 
-export async function publishEvent(event: NDKEvent) {
-	if (import.meta.env.VITEST) {
-		await event.sign()
-	} else {
-		await event.publish()
+export async function publishEvent(event: NDKEvent): Promise<boolean> {
+	try {
+		if (import.meta.env.VITEST) {
+			await event.sign()
+		} else {
+			await event.publish()
+		}
+		return true
+	} catch (error) {
+		console.error('Error publishing event:', error)
+		return false
 	}
 }
