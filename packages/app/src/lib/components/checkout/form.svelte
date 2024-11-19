f1125cf308956b20fdd64aada5458aedd1ff09c6
plebomoto <plebomoto@proton.me>
2024-11-19 16:05:24 +0100

    feat: local pickup conditional addresses

--- packages/app/src/lib/components/checkout/form.svelte
+++ packages/app/src/lib/components/checkout/form.svelte
@@ -4,6 +4,7 @@
 	import { Button } from '$lib/components/ui/button'
 	import { Input } from '$lib/components/ui/input'
 	import { Label } from '$lib/components/ui/label'
+	import Separator from '$lib/components/ui/separator/separator.svelte'
 	import Textarea from '$lib/components/ui/textarea/textarea.svelte'
 	import { checkoutFormSchema } from '$lib/schema'
 	import { checkoutFormStore } from '$lib/stores/checkout'
@@ -27,15 +28,11 @@
 		city: '',
 		country: '',
 		region: '',
-		observations: '',
+		additionalInfo: '',
 	}
 
 	$: if (isLocalPickup) {
-		formData.country = 'Local Pickup'
-		formData.zip = 'Local Pickup'
-		formData.city = 'Local Pickup'
-		formData.address = 'Local Pickup'
-		formData.contactName = 'Local Pickup'
+		formData.additionalInfo = 'Local Pickup'
 	} else {
 		formData.country = ''
 		formData.zip = ''
@@ -43,12 +40,13 @@
 		formData.address = ''
 		formData.contactName = ''
 		formData.contactPhone = ''
+		formData.additionalInfo = ''
 	}
 
 	let validationErrors: ValidationErrors = {}
 
 	function handleSubmit() {
-		validationErrors = validateForm(formData, checkoutFormSchema)
+		validationErrors = isLocalPickup ? {} : validateForm(formData, checkoutFormSchema)
 		if (Object.keys(validationErrors).length === 0) {
 			checkoutFormStore.set(formData)
 			dispatch('validate', { valid: true })
@@ -63,10 +61,17 @@
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
+	{#if isLocalPickup}
+		<div class="flex flex-col gap-2 mb-4">
+			<h3>Local Pickup</h3>
+			<span class="text-gray-500">No need for an address. You can optionally provide name, email address and/or phone number.</span>
+		</div>
+	{/if}
+
 	<Label>
-		<span class="required-mark">{FormLabels.contactName}</span>
+		<span class={isLocalPickup ? '' : 'required-mark'}>{FormLabels.contactName}</span>
 		{#if validationErrors.contactName}<span class="text-red-500">{validationErrors.contactName}</span>{/if}
-		<Input type="text" bind:value={formData.contactName} disabled={isLocalPickup} />
+		<Input type="text" bind:value={formData.contactName} />
 	</Label>
 	<Label>
 		{FormLabels.contactPhone}
@@ -78,35 +83,43 @@
 		{#if validationErrors.contactEmail}<span class="text-red-500">{validationErrors.contactEmail}</span>{/if}
 		<Input type="email" bind:value={formData.contactEmail} />
 	</Label>
+
+	{#if !isLocalPickup}
+		<Separator />
+
+		<Label>
+			<span class="required-mark">{FormLabels.address}</span>
+			{#if validationErrors.address}<span class="text-red-500">{validationErrors.address}</span>{/if}
+			<Input type="text" bind:value={formData.address} />
+		</Label>
+		<Label>
+			<span class="required-mark">{FormLabels.zip}</span>
+			{#if validationErrors.zip}<span class="text-red-500">{validationErrors.zip}</span>{/if}
+			<Input type="text" bind:value={formData.zip} />
+		</Label>
+		<Label>
+			<span class="required-mark">{FormLabels.city}</span>
+			{#if validationErrors.city}<span class="text-red-500">{validationErrors.city}</span>{/if}
+			<Input type="text" bind:value={formData.city} />
+		</Label>
+		<Label>
+			<span class="required-mark">{FormLabels.country}</span>
+			{#if validationErrors.country}<span class="text-red-500">{validationErrors.country}</span>{/if}
+			<Input type="text" bind:value={formData.country} />
+		</Label>
+		<Label>
+			{FormLabels.region}
+			{#if validationErrors.region}<span class="text-red-500">{validationErrors.region}</span>{/if}
+			<Input type="text" bind:value={formData.region} />
+		</Label>
+	{/if}
+
+	<Separator />
+
 	<Label>
-		<span class="required-mark">{FormLabels.address}</span>
-		{#if validationErrors.address}<span class="text-red-500">{validationErrors.address}</span>{/if}
-		<Input type="text" bind:value={formData.address} disabled={isLocalPickup} />
-	</Label>
-	<Label>
-		<span class="required-mark">{FormLabels.zip}</span>
-		{#if validationErrors.zip}<span class="text-red-500">{validationErrors.zip}</span>{/if}
-		<Input type="text" bind:value={formData.zip} disabled={isLocalPickup} />
-	</Label>
-	<Label>
-		<span class="required-mark">{FormLabels.city}</span>
-		{#if validationErrors.city}<span class="text-red-500">{validationErrors.city}</span>{/if}
-		<Input type="text" bind:value={formData.city} disabled={isLocalPickup} />
-	</Label>
-	<Label>
-		<span class="required-mark">{FormLabels.country}</span>
-		{#if validationErrors.country}<span class="text-red-500">{validationErrors.country}</span>{/if}
-		<Input type="text" bind:value={formData.country} disabled={isLocalPickup} />
-	</Label>
-	<Label>
-		{FormLabels.region}
-		{#if validationErrors.region}<span class="text-red-500">{validationErrors.region}</span>{/if}
-		<Input type="text" bind:value={formData.region} disabled={isLocalPickup} />
-	</Label>
-	<Label>
-		{FormLabels.observations}
-		{#if validationErrors.observations}<span class="text-red-500">{validationErrors.observations}</span>{/if}
-		<Textarea bind:value={formData.observations}></Textarea>
+		{FormLabels.additionalInfo}
+		{#if validationErrors.additionalInfo}<span class="text-red-500">{validationErrors.additionalInfo}</span>{/if}
+		<Textarea bind:value={formData.additionalInfo}></Textarea>
 	</Label>
 	<Button type="submit" class="w-full mt-6">Finish Review</Button>
 </form>
