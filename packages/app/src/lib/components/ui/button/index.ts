b9a065c3b7f0e81ee8f24040927d26bd0fe13aa4
plebomoto <plebomoto@proton.me>
2024-12-16 13:35:56 +0100

    refactor: consolidate colors, vix vars and introduce new variants

--- packages/app/src/lib/components/ui/button/index.ts
+++ packages/app/src/lib/components/ui/button/index.ts
@@ -8,11 +8,13 @@ const buttonVariants = tv({
 	base: 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-2 border-black box hover:translated',
 	variants: {
 		variant: {
-			default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+			primary: 'bg-primary text-primary-foreground border-primary-border hover:bg-primary/90',
 			destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
 			outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-			secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-			ghost: 'hover:bg-accent hover:text-accent-foreground',
+			secondary: 'bg-secondary text-secondary-foreground border-secondary-border hover:bg-secondary/80',
+			tertiary: 'bg-tertiary text-tertiary-foreground border-tertiary-border hover:bg-tertiary/80',
+			focus: 'bg-focus text-focus-foreground border-focus-border hover:bg-focus/80',
+			ghost: 'hover:bg-accent border-none hover:text-accent-foreground',
 			link: 'text-primary underline-offset-4 hover:underline',
 		},
 		size: {
@@ -23,7 +25,7 @@ const buttonVariants = tv({
 		},
 	},
 	defaultVariants: {
-		variant: 'default',
+		variant: 'primary',
 		size: 'default',
 	},
 })
@@ -39,12 +41,12 @@ type Props = ButtonPrimitive.Props & {
 type Events = ButtonPrimitive.Events
 
 export {
-	Root,
-	type Props,
-	type Events,
 	//
 	Root as Button,
-	type Props as ButtonProps,
-	type Events as ButtonEvents,
 	buttonVariants,
+	Root,
+	type Events as ButtonEvents,
+	type Props as ButtonProps,
+	type Events,
+	type Props,
 }
