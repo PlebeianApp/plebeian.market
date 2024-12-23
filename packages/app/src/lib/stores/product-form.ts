2608145b0b0a9cc29317523c99d4615e774d00ce
gzuuus <gzuuus@protonmail.com>
2024-12-23 14:02:36 +0100

    refactor: product store state, initialization, and tabs

--- packages/app/src/lib/stores/product-form.ts
+++ packages/app/src/lib/stores/product-form.ts
@@ -1,19 +1,57 @@
+// lib/stores/product-form.ts
 import type { Category } from '$lib/fetch/products.mutations'
 import type { RichShippingInfo } from '$lib/server/shipping.service'
+import type { Writable } from 'svelte/store'
 import { writable } from 'svelte/store'
 
 import type { ProductImage } from '@plebeian/database'
 
-export type ProductFormState = {
-	tab?: 'basic' | 'categories' | 'images' | 'shippings'
+export const PRODUCT_FORM_TABS = ['details', 'categories', 'images', 'shippings'] as const
+export type ProductFormTab = (typeof PRODUCT_FORM_TABS)[number]
+
+export interface ProductFormState {
+	tab: ProductFormTab
 	name: string
 	description: string
 	price: string
 	quantity: string
-	stallIdentifier: string | undefined
+	stallIdentifier?: string
 	categories: Category[]
 	images: Partial<ProductImage>[]
 	shippings: { shipping: Partial<RichShippingInfo> | null; extraCost: string }[]
 }
 
-export const productFormState = writable<ProductFormState | null>(null)
+export const DEFAULT_FORM_STATE: ProductFormState = {
+	tab: 'details',
+	name: '',
+	description: '',
+	price: '',
+	quantity: '',
+	stallIdentifier: undefined,
+	categories: [],
+	images: [],
+	shippings: [],
+}
+
+export function createProductFormStore() {
+	const store: Writable<ProductFormState> = writable(DEFAULT_FORM_STATE)
+
+	return {
+		...store,
+		nextTab: () => {
+			store.update((state) => ({
+				...state,
+				tab: PRODUCT_FORM_TABS[PRODUCT_FORM_TABS.indexOf(state.tab) + 1] || state.tab,
+			}))
+		},
+		previousTab: () => {
+			store.update((state) => ({
+				...state,
+				tab: PRODUCT_FORM_TABS[PRODUCT_FORM_TABS.indexOf(state.tab) - 1] || state.tab,
+			}))
+		},
+		reset: () => store.set(DEFAULT_FORM_STATE),
+	}
+}
+
+export const productFormStore = createProductFormStore()
