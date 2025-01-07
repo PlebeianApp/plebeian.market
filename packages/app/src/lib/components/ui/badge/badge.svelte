6a2ea1e708d06e17ea934e2bda05b1ceda3e30c9
plebomoto <plebomoto@proton.me>
2025-01-07 14:03:17 +0100

    feat: component updates

--- packages/app/src/lib/components/ui/badge/badge.svelte
+++ packages/app/src/lib/components/ui/badge/badge.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import { cn } from '$lib/utils.js'
 
-	import type { Variant } from './index.js'
+	import type { Size, Variant } from './index.js'
 	import { badgeVariants } from './index.js'
 
 	let className: string | undefined | null = undefined
 	export let href: string | undefined = undefined
-	export let variant: Variant = 'default'
+	export let variant: Variant = 'primary'
+	export let size: Size = 'default'
 	export { className as class }
 </script>
 
-<svelte:element this={href ? 'a' : 'span'} {href} class={cn(badgeVariants({ variant, className }))} {...$$restProps}>
+<svelte:element this={href ? 'a' : 'span'} {href} class={cn(badgeVariants({ variant, size, className }))} {...$$restProps}>
 	<slot />
 </svelte:element>
