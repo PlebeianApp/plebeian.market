80aa7db2d89f0784004aa658f63add2792487a52
gzuuus <116975404+gzuuus@users.noreply.github.com>
2025-01-15 06:04:11 +0000

    style: save key dialog (#577)

--- packages/app/src/lib/components/dialogs/saveKeyDialog.svelte
+++ packages/app/src/lib/components/dialogs/saveKeyDialog.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { page } from '$app/stores'
 	import { Button } from '$lib/components/ui/button'
 	import { dialogs } from '$lib/stores/dialog'
 	import { copyToClipboard } from '$lib/utils'
+
+	import Pattern from '../Pattern.svelte'
 
 	export let nsec: string
 
@@ -10,16 +13,20 @@
 	}
 </script>
 
-<div class="flex flex-col gap-4 p-4 max-h-[90vh] overflow-y-auto">
-	<div class="flex flex-col gap-2">
-		<h2 class="font-semibold text-xxl">Save your account key</h2>
-		<p class="text-base">
-			Here is your newly generated account key. It allows you to purchase anonymously, and is stored in your browser on this device only.
-			<span class="font-bold">Be sure to save it, or you'll lose access to your account</span>
-		</p>
+<div class="flex flex-col overflow-y-auto">
+	<div class="relative w-full bg-black text-center py-8 flex items-center justify-center">
+		<Pattern />
+		<div class="flex flex-row gap-2 items-center z-10 px-4">
+			<h2 class="text-secondary">Save your account key</h2>
+		</div>
 	</div>
 
-	<div class="flex flex-col gap-4">
+	<div class="flex flex-col gap-4 p-6">
+		<p class="text-base">
+			Here is your newly generated account key. It allows you to purchase anonymously, and is stored in your browser on this device only.
+		</p>
+		<span class="font-bold">Be sure to save it, or you'll lose access to your account</span>
+
 		<Button
 			variant="secondary"
 			class="relative flex flex-row items-center justify-between gap-2 bg-transparent"
@@ -29,6 +36,6 @@
 			<span class="i-tdesign-copy shrink-0" style="width: 1rem; height: 1rem; color: black;" />
 		</Button>
 
-		<Button variant="focus" on:click={handlePrivKeyConfirmation} class="w-full font-bold">I understand, and I saved my key</Button>
+		<Button variant="focus" on:click={handlePrivKeyConfirmation} class="w-full font-bold">I have saved my key</Button>
 	</div>
 </div>
