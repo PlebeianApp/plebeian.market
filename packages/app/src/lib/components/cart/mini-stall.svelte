7602d721a4c219cf4c08f1a5dd1987e1bc9f5b3a
gzuuus <gzuuus@protonmail.com>
2025-01-30 15:09:55 +0100

    style: updated mini-stall.svelte to add max width to dropdown menu content

--- packages/app/src/lib/components/cart/mini-stall.svelte
+++ packages/app/src/lib/components/cart/mini-stall.svelte
@@ -56,7 +56,7 @@
 						<ChevronDown slot="icon" class="h-4 w-4" />
 					</Button>
 				</DropdownMenu.Trigger>
-				<DropdownMenu.Content class="w-full">
+				<DropdownMenu.Content class="w-full max-w-[30em]">
 					<DropdownMenu.Label>Shipping Method</DropdownMenu.Label>
 					<DropdownMenu.Separator />
 					<section class="max-h-[350px] overflow-y-auto">
