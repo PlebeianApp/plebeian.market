91eff96b5d45ac6370a27be08ac5d1dff963a90a
zazawowow <zazawowow@icloud.com>
2025-02-10 13:58:53 +0000

    refactor: improved the focus style on inputs and text areas

--- packages/app/src/lib/components/ui/input/input.svelte
+++ packages/app/src/lib/components/ui/input/input.svelte
@@ -25,7 +25,7 @@
 	<input
 		bind:this={inputElement}
 		class={cn(
-			'flex h-10 w-full h-full rounded-none border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+			'flex h-10 w-full h-full rounded-none border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus:border-secondary focus:outline-none focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50',
 			className,
 		)}
 		bind:value
